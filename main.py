13b8dbc6f3f8725c4e4a72a0aef744e719289d03
Brearey <brearey4@gmail.com>
2021-11-23 19:39:01 +0900

    checked features of pd.read_excel

--- main.py
+++ main.py
@@ -2,8 +2,15 @@
 # 1. Сделать простой excel файл таблицу с фиктивными данными заболевших covid
 # 2. найти библиотеку для чтения xls xlsx файлов https://pythonru.com/uroki/chtenie-i-zapis-fajlov-excel-xlsx-v-python
 # 3. найти библиотеку для записи xls xlsx файлов
+# 4. объединить два excel файла в один с продолжением номера индекса
 
 import pandas as pd
 
-top_players = pd.read_excel('./testfile.xlsx')
-top_players.head()
+dataframe = pd.read_excel('./testfile.xlsx')
+dataframe.head()
+
+print(dataframe.columns[1])
+print(dataframe.dtypes)
+
+arr = dataframe.values
+print(arr)

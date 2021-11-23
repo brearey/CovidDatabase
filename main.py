60ae17b1b155b7c852364eeab790fd03ead3f3ba
Brearey <brearey4@gmail.com>
2021-11-23 19:22:10 +0900

    installed pip install openpyxl xlsxwriter xlrd

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # todo list
 # 1. Сделать простой excel файл таблицу с фиктивными данными заболевших covid
-# 2. найти библиотеку для чтения xls xlsx файлов
+# 2. найти библиотеку для чтения xls xlsx файлов https://pythonru.com/uroki/chtenie-i-zapis-fajlov-excel-xlsx-v-python
 # 3. найти библиотеку для записи xls xlsx файлов
 
 import pandas as pd
+
+top_players = pd.read_excel('./testfile.xlsx')
+top_players.head()
